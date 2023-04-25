c7adb09da7b80d532fe5743acf75ddae025a550d
Ale Grimminck <a.grimminckfernndez@uandresbello.edu>
2023-04-25 19:05:23 -0400

    refactor(pages): erase all pages for new figma design

--- src/components/Pages/Course/CoursesPage.jsx
+++
@@ -1,8 +0,0 @@
-export const CoursesPage = () => {
-  return (
-    <div className="w-3/4 flex flex-col justify-center items-center">
-      <h1 className='text-2xl font-bold mb-9 text-center'> This is the Courses Page👨🏼‍🎓 </h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, minima.</p>
-    </div>
-  );
-};
