63ec3e1c304ff393f95b8327dd6d8e0e56c75f38
Ale Grimminck <a.grimminckfernndez@uandresbello.edu>
2023-05-10 16:24:10 -0400

    feat(tailwind.config): add figma atoms

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,34 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      white: "#ffffff",
-      "primary-500": "#0C3456",
-      "primary-200": "#5972B2",
+    extend: {
+      fontFamily: {
+        "source-sans": ["Source Sans Pro", "sans-serif"],
+      },
+      fontSize: {
+        sm: ["8px", "18px"],
+        base: ["12px", "22px"],
+        lg: ["16px", "26px"],
+        xl: ["22px", "32px"],
+        "2xl": ["32px", "36px"],
+        "3xl": ["44px", "48px"],
+      },
+      colors: {
+        white: "#ffffff",
+        black: "#000000",
+        "primary-100": "#6B7899",
+        "primary-200": "#5972B2",
+        "primary-400": "#476DCC",
+        "primary-500": "#0C3456",
+        "primary-800": "#002B99",
+        "neutral-100": "#EEEEEE",
+        "neutral-200": "#CACACA",
+        "neutral-400": "#989898",
+        "neutral-600": "#6F6F6F",
+        "neutral-800": "#3C3C3C",
+      },
     },
+
     minWidth: {
       24: "200px",
       25: "830px",
