336070b95855c2998f8e2db50132be87f58ce9d1
javonunezz <javiernunezgaldames@gmail.com>
2023-05-18 19:49:11 -0400

    fix[home] fix responsive design and home elements were added

--- src/components/organisms/pages/page_home/Home.jsx
+++ src/components/organisms/pages/page_home/Home.jsx
@@ -1,11 +1,17 @@
 import { CardTeacher } from "./CardTeacher";
+import { DailySchedule } from "./DailySchedule";
+import { SubjectTeacher } from "./SubjectTeacher";
 
 export const Home = () => {
   return (
-    <>
-      <CardTeacher />
-      <CardTeacher />
-      <CardTeacher />
-    </>
+    <div className="flex flex-wrap justify-center gap-y-10 md:grid md:grid-rows-2 md:gap-y-0">
+      <div className="flex w-full flex-wrap items-center justify-center gap-10 lg:gap-20">
+        <CardTeacher />
+        <DailySchedule />
+      </div>
+      <div className="flex w-screen items-center justify-center">
+        <SubjectTeacher />
+      </div>
+    </div>
   );
 };
